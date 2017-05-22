5291dcc6631518f6851bb03eafedf529d5387ac8
Kenneth Scott <kenneth.scott@georgebrown.ca>
2017-05-21 21:00:55 -0400

    5-21-17

---
+++ Scripts/core/game.js
@@ -0,0 +1,19 @@
+// IIFE - Immediately Invoked Function Expression
+
+(function () {
+    function Start() {
+        console.log("Game started..");
+    }
+
+    function Update() {
+
+    }
+
+    function Main() {
+
+    }
+
+    window.onload = Start;
+
+})();
+
