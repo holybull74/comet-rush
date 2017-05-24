0a0d1386dd4bb02153ff966419de6db742435d3f
Kenneth Scott <kenneth.scott@georgebrown.ca>
2017-05-24 16:49:16 -0400

    Added new player sprites

--- Scripts/core/main-control.js
+++ Scripts/core/main-control.js
@@ -4,135 +4,281 @@
      console.log("Executing script");
 
     var stage = document.getElementById("stage");
-    var canvas = document.querySelector("canvas");
+    var canvas = document.getElementById("myCanvas");
+	canvas.width = 1400;
+    canvas.height = 900;
     var surface = canvas.getContext("2d");
 
+	var leftPressed = false;
+	var rightPressed = false;
+	var jumpPressed = false;
+    
+	var isPressed=false;
+	var end=false;
+	
+	var images = [new Image(), new Image(), new Image(), new Image()];
+	images[0].src = "./Assets/idle-l.png";
+	images[1].src = "./Assets/idle-r.png";
+	images[2].src = "./Assets/run-l.png";
+	images[3].src = "./Assets/run-r.png";
+	var mario = {img:null, x:300, y:600, dir:1, idle:true};
+	
+	var frameIndex = 0; 	// Index of the sprite to display via drawImage.
+	var currentFrame = 0; 	// Counter for the frames.
+	var maxFrames = 3; 		// The number of frames a single sprite is drawn.
 
-    canvas.width = 1400;
-    canvas.height = 900;
-
+	window.addEventListener("keydown", onKeyDown);
+	window.addEventListener("keyup", onKeyUp);
     var topLeft = new Image();
-    topLeft.src = "C:\Users\Ken\George Brown\game1014\Comet-Rush\Assets\TopLeft.png";
+    topLeft.src = "./Assets/TopLeft.png";
 
     var botLeft = new Image();
-    botLeft.src = "BottLeft.png";
+    botLeft.src = "./Assets/BottLeft.png";
 
     var topMid = new Image();
-    topMid.src = "TopMid.png";
+    topMid.src = "./Assets/TopMid.png";
 
     var center = new Image();
-    center.src = "Center.png";
+    center.src = "./Assets/Center.png";
 
     var topRight = new Image();
-    topRight.src = "TopRigth.png";
+    topRight.src = "./Assets/TopRigth.png";
 
     var botRight = new Image();
-    botRight.src = "BottRight.png";
+    botRight.src = "./Assets/BottRight.png";
 
     var lavaTop = new Image();
-    lavaTop.src = "lavaTop.png";
+    lavaTop.src = "./Assets/lavaTop.png";
 
     var lavaBot = new Image();
-    lavaBot.src = "lavaBot.png";
-	
-	var mainChar = new Image();
-    mainChar.src = "MainCharacter.png";
-	
-	
+    lavaBot.src = "./Assets/lavaBot.png";
 
     var offsetX = 0;
     var offsetY = 0;
 
 
 
-    var map =
-        [
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,1,3,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,9,0,0,0,0,0,2,4,6,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [1,3,3,3,5,7,7,1,3,4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,5],
-        [2,4,4,4,6,8,8,2,4,4,4,4,4,4,3,3,3,4,4,4,4,4,4,4,4,3,5],
-        [2,4,4,4,6,8,8,2,4,4,4,4,4,4,3,3,3,4,4,4,4,4,4,4,4,3,5]
-        ];
+    var mapTop =[];
+	var mapBot = [];
+	var SIZE=100;
+	initGame();
+	function initGame()
+	{
+		generateMapTop();
+		generateMapBot();
+	}
+    function generateMapTop()
+{
+	//Generating MAP.
+	for (var i = 0; i < 24; i++) 
+	{
+		
+			
+			var tempTile = { };
+			tempTile.x=i*SIZE;
+			tempTile.y=700;
+			switch(i)
+			{
+				case 0:
+				tempTile.img=topLeft;
+				break;				
+				case 13:
+				case 14:				
+				tempTile.img=lavaTop;
+				break;				
+				case 23:
+				tempTile.img=topRight;
+				
+				break;
+				default:
+				tempTile.img=topMid;
+				break;
+			}
+			mapTop[i] = tempTile;			
+		
+	}
+}
+	function generateMapBot()
+{
+	//Generating MAP.
+	for (var i = 0; i < 24; i++) 
+	{
+		
+			
+			var tempTile = { };
+			tempTile.x=i*SIZE;
+			tempTile.y=800;
+			switch(i)
+			{
+				case 0:
+				tempTile.img=botLeft;
+				break;
+				case 13:
+				case 14:				
+				tempTile.img=lavaBot;
+				break;				
+				case 23:
+				tempTile.img=botRight;
+				break;
+				default:
+				tempTile.img=center;
+				break;
+				
+			}
+			mapBot[i] = tempTile;			
+		
+	}
+}
+	var idInt =	setInterval(update, 33.34);
+function update()
+{	
+	scrollMap();
+	handleInput();
+	animate();
+	render();	
+}
+   function scrollMap()
+{
+	if(isPressed && mario.x>=300)
+	{
+		for(var i = 0; i < 24; i++)
+		{ 
+			mapTop[i].x -= 3;	
+			mapBot[i].x -= 3;
+				if(mapTop[23].x==1298)
+				{	
+					end=true;
+					break;
+				}
+				
+		}
+	}
+}
+function onKeyDown(event)
+{
+	
+	switch(event.keyCode)
+	{
+		case 65: // A
+		case 37: //LEFT
+			if (leftPressed == false)
+				leftPressed = true;
+			break;
+		case 68: // D
+		case 39: //RIGHT
+			isPressed=true;
+			if (rightPressed == false)
+				rightPressed = true;
+			break;
+		case 32:
+		if (jumpPressed == false)
+				jumpPressed = true;
+			jump();
+			break;
+	}
+}
+var n=0;
+function jump()
+{
+	
+	mario.y-=30;
+	
+	n++;
+	
+}
+function onKeyUp(event)
+{
+	isPressed=false;
+	switch(event.keyCode)
+	{
+		case 65: // A
+		case 37: //LEFT
+			leftPressed = false; break;
+		case 68: // D
+		case 39: //RIGHT
+			rightPressed = false; break;
+		case 32:
+		jumpPressed=false;
+		back();
+		break;
+		
+	}
+}
+function back()
+{
+	mario.y+=n*30;
+	
+	n=0;
+	
+}
+function handleInput()
+{
+	if (leftPressed == true)
+	{
+		mario.img = images[2];
+		mario.dir = 1;
+		mario.idle = false;
+		if(mario.x>0)
+		{mario.x += -5;}
+	}
+	else if (rightPressed == true)
+	{
+		mario.img = images[3];
+		mario.dir = -1;
+		mario.idle = false;
+		if(mario.x<300 || (end==true && mario.x<1300))
+		{mario.x += +5;}
+			}
+	else
+	{
+		mario.idle = true;
+		if (mario.dir == 1)
+			mario.img = images[0];
+		else
+			mario.img = images[1];
+	}
+}
 
+function animate()
+{
+	currentFrame++;
+	if (currentFrame == maxFrames)
+	{
+		frameIndex++;
+		currentFrame = 0;
+		if (frameIndex == 4)
+			frameIndex = 0;
+	}
+}
 
-    generateMap();
+	function render()
+{
+	
+	surface.clearRect(0, 0, canvas.width, canvas.height); 
+		for (var col = 0; col < 24; col++)
+		{
+			
+			
+				surface.drawImage(mapBot[col].img,
+								  mapBot[col].x,
+								  mapBot[col].y);				
+		}
+		for (var col = 0; col < 24; col++)
+		{
+			
+			
+				surface.drawImage(mapTop[col].img,
+								  mapTop[col].x,
+								  mapTop[col].y);				
+		}
+		
+		if (mario.idle == true)
+		surface.drawImage(mario.img, mario.x, mario.y);
+	else
+		surface.drawImage(mario.img,
+						  frameIndex*100, 0, 100, 100,		// Source rectangle.
+						  mario.x, mario.y, 100, 100);	// Dest rectangle.
+}
 
-    function generateMap()
-    {
-        for (var row = 0; row < map.length; row++)
-        {
-            for(var col = 0; col < map[0].length; col++)
-            {
-
-                console.log(map[row][col]);
-
-                if(map[row][col] === 1 )
-                {
-                    surface.drawImage(topLeft, col * 100, row * 100);
-                }
-                if(map[row][col] === 2 )
-                {
-                    surface.drawImage(botLeft, col * 100, row * 100);
-                }
-                if(map[row][col] === 3 )
-                {
-                    surface.drawImage(topMid, col * 100, row * 100);
-                }
-                if(map[row][col] === 4 )
-                {
-                    surface.drawImage(center, col * 100, row * 100);
-                }
-                if(map[row][col] === 5 )
-                {
-                    surface.drawImage(topRight, col * 100, row * 100);
-                }
-                if(map[row][col] === 6 )
-                {
-                    surface.drawImage(botRight, col * 100, row * 100);
-                }
-                if(map[row][col] === 7 )
-                {
-                    surface.drawImage(lavaTop, col * 100, row * 100);
-                }
-                if(map[row][col] === 8)
-                {
-                    surface.drawImage(lavaBot, col * 100, row * 100);
-                }
-				if(map[row][col] === 9)
-                {
-                    surface.drawImage(mainChar, col * 100, row * 100);
-                }
-
-            }
-        }
-    }
-
-    function draw()
-    {
-        surface.save();
-        surface.translate(offsetX,offsetY);
-        surface.clearRect(-offsetX, -offsetY, offsetX + 100,900);
-
-        surface.restore();
-
-    }
-
-   window.addEventListener('keydown', function(e) {
-        if (e.keyCode === 37) { // left
-            offsetX--;
-            console.log(offsetX);
-        } else if (e.keyCode === 39) { // right
-            offsetX++;
-            console.log(offsetX);
-        }
-        draw();
-    }, false);
-
-  //  draw();
 })();
 
