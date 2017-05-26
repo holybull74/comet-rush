c8a765afd66a450c5f5bc2d3a51cd3cc3f842368
Kenneth Scott <kenneth.scott@georgebrown.ca>
2017-05-25 22:31:51 -0400

    updated, map layout, cleaned up code

--- Scripts/core/main-control.js
+++ Scripts/core/main-control.js
@@ -3,6 +3,10 @@
 (function () {
     console.log("Executing script");
 
+    const SIZE=100;
+    const ROWS = 4; 
+    const COLS = 72;
+
     var stage = document.getElementById("stage");
     var canvas = document.getElementById("myCanvas");
 	canvas.width = 1400;
@@ -12,6 +16,7 @@
 	var leftPressed = false;
 	var rightPressed = false;
 	var jumpPressed = false;
+    
 	var isPressed=false;
 	var end=false;
 	
@@ -20,12 +25,24 @@
 	images[1].src = "./Assets/idle-r.png";
 	images[2].src = "./Assets/run-l.png";
 	images[3].src = "./Assets/run-r.png";
-	var playerMax = {img:null, x:300, y:600, dir:1, idle:true};
-	
-	var frameIndex = 0; 	// Index of the sprite to display via drawImage.
+
+    var frameIndex = 0; 	// Index of the sprite to display via drawImage.
 	var currentFrame = 0; 	// Counter for the frames.
 	var maxFrames = 3; 		// The number of frames a single sprite is drawn.
 
+    var player = {
+        img: null, x: 300, y: 600, dir: 1, idle: true,
+        animate: function () {
+            currentFrame++;
+            if (currentFrame == maxFrames) {
+                frameIndex++;
+                currentFrame = 0;
+                if (frameIndex == 4)
+                    frameIndex = 0;
+            }
+        }
+    };
+	
 	window.addEventListener("keydown", onKeyDown);
 	window.addEventListener("keyup", onKeyUp);
 
@@ -42,7 +59,7 @@
     center.src = "./Assets/Center.png";
 
     var topRight = new Image();
-    topRight.src = "./Assets/TopRigth.png";
+    topRight.src = "./Assets/TopRight.png";
 
     var botRight = new Image();
     botRight.src = "./Assets/BottRight.png";
@@ -53,87 +70,82 @@
     var lavaBot = new Image();
     lavaBot.src = "./Assets/lavaBot.png";
 
-    var offsetX = 0;
-    var offsetY = 0;
+	var black = new Image();
+    black.src = "./Assets/black.png";
 
-    var mapTop =[];
-	var mapBot = [];
-	var SIZE=100;
+    var map =[
+				[0,0,0,0,0,0,1,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,1,5,0,1,5,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,3,0,0,3,0,0,0,0,0,0,0,0,0,0,0,3,0,0,3,0,0,0,0,0,0,0,0,0,0],
+				[0,0,0,0,0,1,6,0,0,0,0,0,1,5,0,0,0,0,0,0,0,3,0,0,0,0,2,6,0,0,0,0,0,0,0,0,0,3,0,0,0,1,3,6,0,0,0,0,1,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,5,0,0,0,0,0,0],	
+				[1,3,3,3,3,4,6,7,7,7,7,7,2,4,3,3,3,3,3,3,3,3,3,3,3,3,4,6,7,7,7,7,7,7,1,3,3,3,3,3,3,4,4,6,7,7,7,7,2,4,3,3,3,3,3,3,5,7,7,7,7,7,7,7,7,7,7,7,1,3,3,5],
+				[2,4,4,4,4,4,6,8,8,8,8,8,2,4,4,4,4,4,4,4,4,4,4,4,4,4,4,6,8,8,8,8,8,8,2,4,4,4,4,4,4,4,4,6,8,8,8,8,2,4,4,4,4,4,4,4,6,8,8,8,8,8,8,8,8,8,8,8,2,4,4,6]	
+			 ];
 
-	initGame();
     var idInt =	setInterval(update, 33.34);
     var n=0;
 
-	function initGame() {
-		generateMapTop();
-		generateMapBot();
-	}
+	initGame();
 
-    function generateMapTop() {
-        //Generating MAP.
-        for (var i = 0; i < 24; i++) {
-            var tempTile = {};
-            tempTile.x = i * SIZE;
-            tempTile.y = 700;
-            switch (i) {
-                case 0:
-                    tempTile.img = topLeft;
-                    break;
-                case 13:
-                case 14:
-                    tempTile.img = lavaTop;
-                    break;
-                case 23:
-                    tempTile.img = topRight;
-                    break;
-                default:
-                    tempTile.img = topMid;
-                    break;
-            }
-            mapTop[i] = tempTile;
-        }
+    function initGame() {
+        generateMap();
     }
 
-    function generateMapBot() {
+    function generateMap() {
         //Generating MAP.
-        for (var i = 0; i < 24; i++) {
-            var tempTile = {};
-            tempTile.x = i * SIZE;
-            tempTile.y = 800;
-            switch (i) {
-                case 0:
-                    tempTile.img = botLeft;
-                    break;
-                case 13:
-                case 14:
-                    tempTile.img = lavaBot;
-                    break;
-                case 23:
-                    tempTile.img = botRight;
-                    break;
-                default:
-                    tempTile.img = center;
-                    break;
+        for (var row = 0; row < ROWS; row++) {
+            for (var col = 0; col < COLS; col++) {
+                var tempTile = {};
+                tempTile.x = col * SIZE;
+                tempTile.y = (5 + row) * SIZE;
+                switch (map[row][col]) {
+                    case 0:
+                        tempTile.img = black;
+                        break;
+                    case 1:
+                        tempTile.img = topLeft;
+                        break;
+                    case 2:
+                        tempTile.img = botLeft;
+                        break;
+                    case 3:
+                        tempTile.img = topMid;
+                        break;
+                    case 4:
+                        tempTile.img = center;
+                        break;
+                    case 5:
+                        tempTile.img = topRight;
+                        break;
+                    case 6:
+                        tempTile.img = botRight;
+                        break;
+                    case 7:
+                        tempTile.img = lavaTop;
+                        break;
+                    case 8:
+                        tempTile.img = lavaBot;
+                        break;
+                }
+                map[row][col] = tempTile;
             }
-            mapBot[i] = tempTile;
         }
-    }
+    } 
 	
-    function update() {	
+    function update() {
         scrollMap();
         handleInput();
         animate();
-        render();	
+        render();
     }
 
     function scrollMap() {
-        if (isPressed && playerMax.x >= 300) {
-            for (var i = 0; i < 24; i++) {
-                mapTop[i].x -= 3;
-                mapBot[i].x -= 3;
-                if (mapTop[23].x == 1298) {
-                    end = true;
-                    break;
+        if (isPressed && player.x >= 300) {
+            for (var row = 0; row < map.length; row++) {
+                for (var col = 0; col < map[0].length; col++) {
+                    map[row][col].x -= 5;
+                    if (map[map.length - 1][map[0].length - 1].x == 1300) {
+                        end = true;
+                        break;
+                    }
                 }
             }
         }
@@ -161,10 +173,10 @@
     }
 
     function jump() {
-        playerMax.y -= 30;
+        player.y -= 30;
         n++;
     }
-    
+
     function onKeyUp(event) {
         isPressed = false;
         switch (event.keyCode) {
@@ -182,65 +194,56 @@
     }
 
     function back() {
-        playerMax.y += n * 30;
+        player.y += n * 30;
         n = 0;
     }
 
     function handleInput() {
         if (leftPressed == true) {
-            playerMax.img = images[2];
-            playerMax.dir = 1;
-            playerMax.idle = false;
-            if (playerMax.x > 0) 
-                playerMax.x += -5;
+            player.img = images[2];
+            player.dir = 1;
+            player.idle = false;
+            if (player.x > 0)
+            { player.x -= 5; }
         }
         else if (rightPressed == true) {
-            playerMax.img = images[3];
-            playerMax.dir = -1;
-            playerMax.idle = false;
-            if (playerMax.x < 300 || (end == true && playerMax.x < 1300)) {
-                playerMax.x += +5;
-            }
+            player.img = images[3];
+            player.dir = -1;
+            player.idle = false;
+            if (player.x < 300 || (end == true && player.x < 1300))
+            { player.x += 5; }
         }
         else {
-            playerMax.idle = true;
-            if (playerMax.dir == 1)
-                playerMax.img = images[0];
+            player.idle = true;
+            if (player.dir == 1)
+                player.img = images[0];
             else
-                playerMax.img = images[1];
+                player.img = images[1];
         }
     }
 
     function animate() {
-        currentFrame++;
-        if (currentFrame == maxFrames) {
-            frameIndex++;
-            currentFrame = 0;
-            if (frameIndex == 4)
-                frameIndex = 0;
-        }
+        setTimeout(animate, 1000);
+        player.animate();
     }
 
     function render() {
         surface.clearRect(0, 0, canvas.width, canvas.height);
-        for (var col = 0; col < 24; col++) {
-            surface.drawImage(  mapBot[col].img,
-                                mapBot[col].x,
-                                mapBot[col].y);
+        for (var row = 0; row < map.length; row++) {
+            for (var col = 0; col < map[0].length; col++) {
+                surface.drawImage(  map[row][col].img,
+                                    map[row][col].x,
+                                    map[row][col].y);
+            }
         }
-        for (var col = 0; col < 24; col++) {
-            surface.drawImage   (mapTop[col].img,
-                                mapTop[col].x,
-                                mapTop[col].y);
-        }
-        if (playerMax.idle == true)
-            surface.drawImage(  playerMax.img, 
-                                playerMax.x, 
-                                playerMax.y);
+        if (player.idle == true)
+            surface.drawImage(  player.img, 
+                                player.x, 
+                                player.y);
         else
-            surface.drawImage(  playerMax.img,
+            surface.drawImage(  player.img,
                                 frameIndex * 100, 0, 100, 100,		// Source rectangle.
-                                playerMax.x, playerMax.y, 100, 100);	// Dest rectangle.
+                                player.x, player.y, 100, 100);	// Dest rectangle.
     }
 })();
 
